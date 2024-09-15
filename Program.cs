3ab56c77f2ae3d607b7b02f4423b7a95490143a9
Ivan93LA <ivan.inln17@gmail.com>
2024-09-15 21:52:22 +0200

    Mas apuntes y correcciones

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSharpHelloWorld {
 
     class HelloWorld
     {
         static void Main(string[] args)
-        { 
+        {
             // Ejemplo de un comentario
             Console.WriteLine("Hola mi mundo con el lenguaje C#");
             /*
@@ -24,23 +25,46 @@ namespace CSharpHelloWorld {
             Console.WriteLine(myInt - 1);
             Console.WriteLine(myInt);
 
-            float myFlotat = 6.8f;
+            float myFloat = 6.8f;
             //Para definir el decimal, SI o SI tenemos que poner el "f"
-             Console.WriteLine(myFlotat);
+            Console.WriteLine(myFloat);
 
             //Pero si lo defines como double, si lo reconoce
             double myDouble = 6.8;
             Console.WriteLine(myDouble);
 
+            Console.WriteLine(myInt + myFloat + myDouble);
 
+            dynamic myDynamic = 6;
+            myDynamic = "Mi dato dinamico";
+            Console.WriteLine(myDynamic);
+
+            //ESTRUCTURAS
+            //Los arrays tienen una longitud fija ni tampoco cambiarles el tipo
+            var myArray = new string[] { "Ivan", "Leon", "Alvarez" };
+            Console.WriteLine(myArray[0]);
+
+
+            myArray[2] = "31";
+            Console.WriteLine(myArray[2]);
+
+
+            var myDiccionario = new Dictionary<string, int>
+            { 
+                { "Ivan" , 31},
+                { "Paula" , 25}
+
+             };
+
+            Console.WriteLine(myDiccionario["Paula"]);
+
+            
+            
 
 
 
         }
     }
-
-    
-
 
 
 }
--- obj/Debug/net8.0/practicanet.AssemblyInfo.cs
+++ obj/Debug/net8.0/practicanet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("practicanet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa7bb519e232f0226fbb1f4b9cf5ea763e08555f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b633da50d75059e633e9b8be88846e9ff3c6113a")]
 [assembly: System.Reflection.AssemblyProductAttribute("practicanet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("practicanet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
